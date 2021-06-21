247777fc7ae860bd2c1e8153605c693f264c0a0d
Szczur3k <Mateuszlukaszczyk13@gmail.com>
2021-06-21 15:10:27 +0200

    Added basic HUD (I think its not the best implementation but it works)

--- AllProgramowanie/WszystkieAplikacje/GameWave/src/sample/Game.java
+++ AllProgramowanie/WszystkieAplikacje/GameWave/src/sample/Game.java
@@ -13,6 +13,7 @@ public class Game extends Canvas implements Runnable {
     private boolean running = false;
     private final Handler handler;
     private final Random r = new Random();
+    private final HUD hud;
 
     public Game() {
         //Handler is must to initialize beacuse we call it in render() method
@@ -22,6 +23,8 @@ public class Game extends Canvas implements Runnable {
         this.addKeyListener(new KeyInput(handler));
 
         new Window(WIDTH, HEIGHT, "Let's Build a Game", this);
+
+        hud = new HUD();
 
         //Set object to handler. Player is created and given to the stage
         handler.addObject(new Player(WIDTH / 2 - 32, HEIGHT / 2 - 32, ID.PLAYER));
@@ -52,6 +55,8 @@ public class Game extends Canvas implements Runnable {
     //Thats method for calling FPS, times and keep everything fluent
     @Override
     public void run() {
+        //request focus focus on the window so you dont need to click it to move player or anything else
+        this.requestFocus();
         long lastTime = System.nanoTime();
         double amountOfTicks = 60.0;
         double ns = 1000000000 / amountOfTicks;
@@ -94,6 +99,7 @@ public class Game extends Canvas implements Runnable {
         g.fillRect(0, 0, WIDTH, HEIGHT);
 
         handler.render(g);
+        hud.render(g);
 
         g.dispose();
         bs.show();
@@ -101,6 +107,7 @@ public class Game extends Canvas implements Runnable {
 
     private void tick() {
         handler.tick();
+        hud.tick();
     }
 
     public static int clamp(int variable, int min, int max) {
